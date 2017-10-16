e008249fccf62fb5573d3e90f230b8b9dafb2adc
Adam Clayman <adam@clayman.org>
2017-10-16 04:02:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |name|
+    badge_array.push("Hello, my name is #{name}.")
+  end
+  return badge_array
+end
