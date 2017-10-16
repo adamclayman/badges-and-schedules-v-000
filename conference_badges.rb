72c5464be18b2a3f2b84c58e40eda54af6e88de5
Adam Clayman <adam@clayman.org>
2017-10-16 04:08:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,23 @@ def batch_badge_creator(array)
   end
   return badge_array
 end
+
+def assign_rooms(array)
+  room_count = 1
+  room_assignments = []
+  array.each do |name|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{room_count}"!)
+    room_count +=1
+  end
+  return room_assignments
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(array).each do |assignment|
+    puts assignment
+  end
+end
